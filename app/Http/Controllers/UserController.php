ee7522fae264e9dc2d0641715477aa908f4893e6
YoussefLMT <mcyoussef324@gmail.com>
2022-11-05 18:31:30 +0100

    add destroy function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -118,6 +118,13 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+
+        $user->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Deleted successfully',
+        ]);
     }
 }
